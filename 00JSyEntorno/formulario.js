dcda71ccb2d0b1363500fc7ce115cc9d28ffc252
sinbelisk <=rafjimray21y@proton.me>
2026-09-24 10:04:54 +0200

    refactor

--- 00JSyEntorno/formulario.js
+++ 00JSyEntorno/formulario.js
@@ -1,8 +1,17 @@
+// DOMContentLoaded: Primer evento, espera a que el html se cargue
+// antes de ejecutar el código.
+
+// Esta función es un "listener" (un tipo de patrón de diseño)
+// Espera a que se dispare el evento para ejecutar el código
 document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("formulario")
+
+    // Segundo evento "submit", se dispara cuando el usuario envía el
+    // formulario.
+    // Evita que se recargue la página   .addEventListener("submit", function (event) {
     .addEventListener("submit", function (event) {
-      event.preventDefault();
+      event.preventDefault(); // Evita que se recargue la página
       const nombre = document.getElementById("nombreInput").value;
       document.getElementById("salida").textContent = "¡Hola, " + nombre + "!";
     });
--- README.md
+++ README.md
@@ -166,8 +166,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Segundo evento "submit", se dispara cuando el usuario envía el
     // formulario.
-  // Evita que se recargue la página   .addEventListener("submit", function (event) {
-    
+    // Evita que se recargue la página   .addEventListener("submit", function (event) {
+    .addEventListener("submit", function (event) {
       event.preventDefault(); // Evita que se recargue la página
       const nombre = document.getElementById("nombreInput").value;
       document.getElementById("salida").textContent = "¡Hola, " + nombre + "!";
